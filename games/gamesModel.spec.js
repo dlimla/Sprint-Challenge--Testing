614273df903eca06bdc96d3c3e7f72d60be8433f
dlimla <danielhlim1990@gmail.com>
2019-04-20 10:50:22 -0700

    adds some test for the model handlers

--- games/gamesModel.spec.js
+++ games/gamesModel.spec.js
@@ -20,8 +20,34 @@ describe('the insert fn', () => {
         expect(games.length).toBe(1);
         expect(games[0].title).toBe('Thief');
     })
+
+    it('should insert a game with an id', async () => {
+        const games = await Games.insert({
+            title: 'Thief',
+            genre: 'stealth',
+            releaseYear: 2014
+        })
+
+        expect(games.id).toBe(1);
+        expect(games.title).toBe('Thief');
+    })
 })
 
-// describe('the getAll fn', async () => {
+describe('the getAll fn', async () => {
+    beforeEach(()=> {
+        return db('games').truncate();
+    })
 
-// })
+    it('should get all games in the db', async () => {
+        await db('games').insert([
+            { title: 'Mario', genre: 'arcade', releaseYear: 3242 }
+        ])
+
+        const games = await Games.getAll();
+
+        expect(games.length).toBe(1);
+        expect(games[0].title).toBe('Mario');
+        expect(games[0].id).toBe(1);
+    })
+
+})
